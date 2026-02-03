a54a517a42b932ebd871d21f81cfc4489fca8933
RevenBro <saidakbarov077@gmail.com>
2026-02-04 00:07:55 +0500

    Fix: Use shared models from npm package

--- handlers/premiumHandler.js
+++ handlers/premiumHandler.js
@@ -1,5 +1,5 @@
 // bot/handlers/premiumHandler.js
-const User = require('../models/User');
+const { User } = require('@revencoder/anonymous-shared');
 const TelegramBot = require('node-telegram-bot-api');
 
 /**
